a31406f703b906af85e216cd68b35d8a438bf039
nurlanorazkeldiyev <nurlan_o.s@mail.ru>
2019-02-22 09:47:56 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-class Customer(object):
+from myclass import MyClass
+    
 
-def __init__(self, name, balance=0.0):
-
-self.name = name
-self.balance = balance
-
-def withdraw(self, amount):
-
-if amount > self.balance:
-raise RuntimeError('Amount greater than available balance.')
-self.balance -= amount
-return self.balance
-
-def deposit(self, amount):
-
-self.balance += amount
-return self.balance
+a = MyClass("Kazakhstan","Taraz","march")
+a.meth_1()
+a.meth_2()
+a.meth_1()
